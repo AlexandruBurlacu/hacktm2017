3ce9b3f1aa8d8425a081ad0eee07f7d440745805
Alexandru Burlacu <alexburlacu96@gmail.com>
2017-05-27 20:30:35 +0300

    update api.py to work with links

--- service/api.py
+++ service/api.py
@@ -9,10 +9,5 @@ vision.init()
 @hug.post("/isAuthorized") 
 def isAuthorized(image1, image2):
 
-  with open("temp_img1.jpg", "wb") as img1, \
-       open("temp_img2.jpg", "wb") as img2:
-    img1.write(image1)
-    img2.write(image2)
-  
-    return vision.areSimilar(img1, img2)
+    return vision.areSimilar(image1, image2)
 
