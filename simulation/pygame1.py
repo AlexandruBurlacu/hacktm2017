7a59ea54136af5e4e0f76f4134fdc7570dd1ce78
Iura <iura.marusic@gmail.com>
2017-05-28 13:46:57 +0300

    fixed jpg

--- simulation/pygame1.py
+++ simulation/pygame1.py
@@ -7,17 +7,17 @@ cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
 
 
 frames = 1
-while (frames <= 2):
+while (frames <= 1):
 	cam.start()
 	img = cam.get_image()
-	pygame.image.save(img, "photo"+str(frames)+".bmp")
+	pygame.image.save(img, "photo"+str(frames)+".jpg")
 	pygame.camera.quit()
 	cam.stop()
-	time.sleep(1)
+	# time.sleep(1)
 	frames = frames + 1
 	
 
 import requests
-url = 'http://10.37.95.16:3000/validate_driver'
-files = {'media': open('photo1.bmp', 'rb')}
+url = 'http://92.222.66.166:3000/validate_driver'
+files = {'photo': open('photo1.jpg', 'rb')}
 requests.put(url, files=files)
